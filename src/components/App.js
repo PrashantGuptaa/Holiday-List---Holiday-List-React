697aebb183c1b57d9f6e9437f5ff515e56c36a00
Prashant Gupta <68388206+PrashantGuptaa@users.noreply.github.com>
2020-10-21 15:41:36 +0530

    NS Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,19 @@ import '../styles/App.css';
 
 class App extends Component {
     render() {
-
-        return(
-           <>
-//write your code here
-	</>
-        )
+      const cities = ["abc", "bcd", "cde"];
+      return (
+        <>
+          <ol>
+            {cities.map((city, index) => (
+              <li key={`location${index + 1} `}>{city}</li>
+            ))}
+          </ol>
+        </>
+      );
     }
-}
+  }
+  
 
 
 export default App;
